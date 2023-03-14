0e44caa87ccac030c98d6cb96f74bd4f09d63ebf
progML <matveylesonen@gmail.com>
2023-03-14 21:01:41 +0300

    Time: 2 ms (64.29%), Space: 40.9 MB (20.50%)

---
+++ src/Easy/ValidParentheses/Solution.java
@@ -0,0 +1,25 @@
+package Easy.ValidParentheses;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> leftSymbols = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                leftSymbols.push(c);
+            }
+            else if (c == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
+                leftSymbols.pop();
+            } else if (c == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+                leftSymbols.pop();
+            } else if (c == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+                leftSymbols.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return leftSymbols.isEmpty();
+    }
+}
