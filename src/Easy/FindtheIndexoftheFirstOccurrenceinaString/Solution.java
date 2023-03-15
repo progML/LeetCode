b22c30b09f1f342abcd847f5e274d2d77da35b37
progML <matveylesonen@gmail.com>
2023-03-15 14:30:02 +0300

    Time: 0 ms (100%), Space: 40.9 MB (19.14%)

---
+++ src/Easy/FindtheIndexoftheFirstOccurrenceinaString/Solution.java
@@ -0,0 +1,7 @@
+package Easy.FindtheIndexoftheFirstOccurrenceinaString;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
