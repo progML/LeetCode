493b7a23aeaa76acab73faa6a0f60f69a4b9277d
progML <matveylesonen@gmail.com>
2023-03-17 13:34:18 +0300

    Time: 167 ms (38.95%), Space: 44.1 MB (11.24%)

---
+++ src/Easy/ValidPalindrome/Solution.java
@@ -0,0 +1,15 @@
+package Easy.ValidPalindrome;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        while (s.length() > 1) {
+            if (s.charAt(0) != s.charAt(s.length() - 1)) {
+                return false;
+            } else
+                s = s.substring(1, s.length() - 1);
+        }
+        return true;
+    }
+
+}
