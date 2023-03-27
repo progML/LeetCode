52b7873fa855aca7b4e86006b54d61647ef19a67
progML <matveylesonen@gmail.com>
2023-03-27 13:21:33 +0300

    Time: 11 ms (50.32%), Space: 42.8 MB (19.59%)

---
+++ src/Easy/IsomorphicStrings/Solution.java
@@ -0,0 +1,12 @@
+package Easy.IsomorphicStrings;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
