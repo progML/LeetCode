9570f4b1e3280dfbbc65494e3dde209d863e19c3
progML <matveylesonen@gmail.com>
2023-03-29 20:27:49 +0300

    Time: 2 ms (81.88%), Space: 42.7 MB (52.81%)

--- src/Easy/TwoSum/Solution.java
+++ src/Easy/TwoSum/Solution.java
@@ -1,18 +1,18 @@
 package Easy.TwoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    res[0] = i;
-                    res[1] = j;
-                    return res;
-                }
+            int difference = target - nums[i];
+            if (map.containsKey(difference)) {
+                return new int[]{i, map.get(difference)};
             }
+            map.put(nums[i], i);
         }
-        return res;
+        return null;
     }
-
 }
