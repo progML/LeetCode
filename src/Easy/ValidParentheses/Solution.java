b1cca1d49a7fb51bdd8c9262bbf70bbd0fddd86f
progML <matveylesonen@gmail.com>
2023-03-31 13:33:30 +0300

    Time: 11 ms (25.16%), Space: 42.4 MB (23.27%)

--- src/Easy/ValidParentheses/Solution.java
+++ src/Easy/ValidParentheses/Solution.java
@@ -1,25 +1,14 @@
 package Easy.ValidParentheses;
 
-import java.util.Stack;
-
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> leftSymbols = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                leftSymbols.push(c);
-            }
-            else if (c == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
-                leftSymbols.pop();
-            } else if (c == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
-                leftSymbols.pop();
-            } else if (c == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
-                leftSymbols.pop();
-            }
-            else {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        while (s.length() > 1) {
+            if (s.charAt(0) != s.charAt(s.length() - 1)) {
                 return false;
-            }
+            } else
+                s = s.substring(1, s.length() - 1);
         }
-        return leftSymbols.isEmpty();
+        return true;
     }
 }
