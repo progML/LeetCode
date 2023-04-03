d17360e03d4250c9a6678315391befd4122850e5
progML <matveylesonen@gmail.com>
2023-04-03 13:46:46 +0300

    Time: 2 ms (74.25%), Space: 40.6 MB (37.56%)

--- src/Easy/ValidParentheses/Solution.java
+++ src/Easy/ValidParentheses/Solution.java
@@ -1,14 +1,27 @@
 package Easy.ValidParentheses;
 
+import java.util.Stack;
+
 class Solution {
-    public boolean isPalindrome(int x) {
-        int reverse = 0;
-        int temp = x;
-        while (x > 0) {
-            reverse = reverse * 10 + x % 10;
-            x /= 10;
+    public boolean isValid(String s) {
+        Stack<Character> leftSymbols = new Stack<>();
+        for (char symbol : s.toCharArray()) {
+            if (symbol == '(' || symbol == '{' || symbol == '[') {
+                leftSymbols.push(symbol);
+            } else if (!leftSymbols.isEmpty()) {
+                if (symbol - 1 == leftSymbols.peek() || symbol - 2 == leftSymbols.peek())
+                    leftSymbols.pop();
+                else
+                    return false;
+            } else
+                return false;
         }
-        return temp == reverse;
+        return leftSymbols.isEmpty();
     }
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("(])"));
+    }
 }
+
