9aeab2db21cba9b16aea98eddd905861850e7251
progML <matveylesonen@gmail.com>
2023-04-03 15:06:06 +0300

    Time: 243 ms (37.51%), Space: 43.5 MB (32.23%)

--- src/Easy/ValidPalindrome/Solution.java
+++ src/Easy/ValidPalindrome/Solution.java
@@ -3,12 +3,14 @@ package Easy.ValidPalindrome;
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        while (s.length() > 1) {
-            if (s.charAt(0) != s.charAt(s.length() - 1)) {
-                return false;
-            }else
-                s = s.substring(1, s.length() - 1);
+        StringBuilder temp = new StringBuilder();
+        if (s.length() == 1) return true;
+        for (int i = s.length() - 1; i > 0; i--) {
+            temp.append(s.charAt(i));
+            s = s.substring(0, i);
+            if (s.length() <= temp.length())
+                break;
         }
-        return true;
+        return s.contentEquals(temp) || s.contentEquals(temp.substring(0, temp.length() - 1));
     }
 }
