39532cf255d8fe54ac175c7830309670ed58d436
progML <matveylesonen@gmail.com>
2023-04-04 17:14:42 +0300

    Time: 1 ms (99.97%), Space: 44 MB (54.17%)

--- src/Easy/RemoveDuplicatesfromSortedArray/Solution.java
+++ src/Easy/RemoveDuplicatesfromSortedArray/Solution.java
@@ -4,29 +4,13 @@ import java.util.Stack;
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Stack<Integer> heap = new Stack<>();
-        int[] dummy = new int[nums.length];
-        int k = 1;
-        heap.push(nums[0]);
-        dummy[0] = nums[0];
-        if (nums.length < 2) {
-            nums[0] = heap.get(0);
-            return k;
-        } else {
-            for (int i = 1; i < dummy.length; i++) {
-                if (heap.peek() != nums[i]) {
-                    heap.push(nums[i]);
-                    k++;
-                }
+        int index = 0;
+        if (nums.length == 1) return index + 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[index] != nums[i]) {
+                nums[++index] = nums[i];
             }
         }
-        for (int i = 1; i < dummy.length; i++) {
-            if (heap.size() > i)
-                nums[i] = heap.get(i);
-            else
-                nums[i] = ' ';
-        }
-        return k;
+        return index + 1;
     }
-
 }
